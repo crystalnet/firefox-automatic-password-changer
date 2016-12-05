b6ef44b186d76cc921c69590bd565fb1e8a5d80a
Benedikt Gross <bgfodw@gmx.de>
2016-12-05 18:28:46 +0100

    added asser.announce() to asserExtensions for unified "test benig:", fixed all test for Recorder.js

--- test/test-lib-recorder.js
+++ test/test-lib-recorder.js
@@ -35,12 +35,14 @@ var countAllChildrenOfType_testdata = (function () {
 })();
 
 exports["test recorder isactive should initially be false"] = function (assert) {
+    assert.announce("test recorder isactive should initially be false");
     var recorder = new Recorder();
 
     assert.shouldBe(recorder.RecorderIsActive(), false);
 };
 
 exports["test recorder getMainPageFromLink should extract host"] = function (assert) {
+    assert.announce("test recorder getMainPageFromLink should extract host");
     var recorder = new Recorder();
     var result = recorder.testhook.getMainPageFromLink("https://www.facebook.com/settings/password");
 
@@ -49,17 +51,20 @@ exports["test recorder getMainPageFromLink should extract host"] = function (ass
 
 // countAllChildrenOfType tests
 exports["test recorder countAllChildrenOfType should find top-layer"] = function (assert) {
+    assert.announce("test recorder countAllChildrenOfType should find top-layer");
     var recorder = new Recorder();
     var result = recorder.testhook.countAllChildrenOfType(countAllChildrenOfType_testdata.rootNode, "notpw");
     assert.shouldBe(result, 2);
 };
 exports["test recorder countAllChildrenOfType should find in children"] = function (assert) {
+    assert.announce("test recorder countAllChildrenOfType should find in children");
     var recorder = new Recorder();
     var result = recorder.testhook.countAllChildrenOfType(countAllChildrenOfType_testdata.rootNode, "password");
 
     assert.shouldBe(result, 1);
 };
 exports["test recorder countAllChildrenOfType should find self"] = function (assert) {
+    assert.announce("test recorder countAllChildrenOfType should find self");
     // this test was not working in version 0.4
     var recorder = new Recorder();
     var result = recorder.testhook.countAllChildrenOfType(countAllChildrenOfType_testdata.rootNode, "roottype");
@@ -67,6 +72,7 @@ exports["test recorder countAllChildrenOfType should find self"] = function (ass
     assert.shouldBe(result, 2);
 };
 exports["test recorder countAllChildrenOfType should work with not existing type"] = function (assert) {
+    assert.announce("test recorder countAllChildrenOfType should work with not existing type");
     var recorder = new Recorder();
     var result = recorder.testhook.countAllChildrenOfType(countAllChildrenOfType_testdata.rootNode, "NotAType");
 
@@ -154,7 +160,8 @@ var tableEntry_header = [submitOrClick_testdata.form.id,
 function test_one_message_combination_for(recorder, assert, injectedHashTable,
                                           usernameSet, actualpasswordSet, newpasswordSet,
                                           expectedInsert, index, executeFunction) {
-
+    // TODO check why submitSeen behaves differently in onSubmit and onClick
+    recorder.testhook.resetSubmitSeen();
     var combinationToString = usernameSet + ":" + actualpasswordSet + ":" + newpasswordSet;
     if (usernameSet) {
         recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubBE1);
@@ -165,7 +172,6 @@ function test_one_message_combination_for(recorder, assert, injectedHashTable,
     if (newpasswordSet) {
         recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubNP3);
     }
-
     executeFunction();
     // test if it inserted the expected result
     assert.sequenceShouldBe(injectedHashTable.getItem(index), tableEntry_header.concat(expectedInsert), "combination is " + combinationToString);
@@ -218,12 +224,14 @@ function test_orderNumber_increase_for_all_message_combinations(recorder, assert
 
     // set just actualPassword
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubAP2);
+    recorder.testhook.resetSubmitSeen();
     executeFunction();
     var keys = hashtable.keys();
     assert.shouldBe(keys[keys.length - 1], i++);
 
     // set just username-email
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubBE1);
+    recorder.testhook.resetSubmitSeen();
     executeFunction();
     var keys = hashtable.keys();
     assert.shouldBe(keys[keys.length - 1], i++);
@@ -231,17 +239,20 @@ function test_orderNumber_increase_for_all_message_combinations(recorder, assert
     // set actualPassword and newPassword
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubAP2);
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubNP3);
+    recorder.testhook.resetSubmitSeen();
     executeFunction();
     var keys = hashtable.keys();
     assert.shouldBe(keys[keys.length - 1], i++);
 
     // set just newPassword
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubNP3);
+    recorder.testhook.resetSubmitSeen();
     executeFunction();
     var keys = hashtable.keys();
     assert.shouldBe(keys[keys.length - 1], i++);
 
     // no field set
+    recorder.testhook.resetSubmitSeen();
     executeFunction();
     var keys = hashtable.keys();
     assert.shouldBe(keys[keys.length - 1], i++);
@@ -249,12 +260,14 @@ function test_orderNumber_increase_for_all_message_combinations(recorder, assert
 
 // onSubmit tests
 exports["test recorder onSubmit actual password and username email fields set"] = function (assert) {
+    assert.announce("test recorder onSubmit actual password and username email fields set");
     var recorder = new Recorder();
     test_all_message_combinations_for(recorder, assert, function () {
         recorder.testhook.onSubmit(submitOrClick_testdata.eventStub);
     });
 };
 exports["test recorder onSubmit orderNumber should be increased"] = function (assert) {
+    assert.announce("test recorder onSubmit orderNumber should be increased");
     var recorder = new Recorder();
     test_orderNumber_increase_for_all_message_combinations(recorder, assert, function () {
         recorder.testhook.onSubmit(submitOrClick_testdata.eventStub);
@@ -263,12 +276,14 @@ exports["test recorder onSubmit orderNumber should be increased"] = function (as
 
 // onClick tests
 exports["test recorder onClick actual password and username email fields set"] = function (assert) {
+    assert.announce("test recorder onClick actual password and username email fields set");
     var recorder = new Recorder();
     test_all_message_combinations_for(recorder, assert, function () {
         recorder.testhook.onClick(submitOrClick_testdata.onClickEventStub);
     });
 };
 exports["test recorder onClick orderNumber should be increased"] = function (assert) {
+    assert.announce("test recorder onClick orderNumber should be increased");
     var recorder = new Recorder();
     test_orderNumber_increase_for_all_message_combinations(recorder, assert, function () {
         recorder.testhook.onClick(submitOrClick_testdata.onClickEventStub);
@@ -276,6 +291,7 @@ exports["test recorder onClick orderNumber should be increased"] = function (ass
 };
 
 exports["test recorder onClickOnSubmit no password fields"] = function (assert) {
+    assert.announce("test recorder onClickOnSubmit no password fields");
     var recorder = new Recorder();
     var hashtable = new HashTable();
     recorder.testhook.injectTabs(submitOrClick_testdata.tabs);
@@ -301,7 +317,7 @@ exports["test recorder onClickOnSubmit no password fields"] = function (assert) 
     // TODO: check if this is the correct behavior
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubAP2);
     recorder.testhook.setMessageValues(submitOrClick_testdata.messageStubBE1);
-    recorder.testhook.onSubmit({target: submitOrClick_testdata.formWithoutPasswordField})
+    recorder.testhook.onSubmit({target: submitOrClick_testdata.formWithoutPasswordField});
     assert.shouldBe(hashtable.length, 1);
 };
 
