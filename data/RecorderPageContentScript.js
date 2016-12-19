d164d074040071257c28a8dae9a294b8d5c1325a
Johannes <wagener.johannes@googlemail.com>
2016-12-19 01:34:00 +0100

    - Fixed fetching of input items: The value of an input element is now fetched even if a user first tags the input element and then enters a value -> as a result of this fix, creating blueprints for Google is now working - Renamed ContextMenuScript.js for consistency reasons

--- data/RecorderPageContentScript.js
+++ data/RecorderPageContentScript.js
@@ -3,7 +3,7 @@ This is a content script that the recorder attaches to a webpage at recording a 
 if contextmenu is used an inputfield will be marked yellow as visual feedback for user
 */
 self.port.on("ContextMenuClick", function(message){
-    var position = message[3];
+    var position = message[0];
     var inputs = document.getElementsByTagName("input");
     inputs[position].style.backgroundColor = "yellow";
 });
