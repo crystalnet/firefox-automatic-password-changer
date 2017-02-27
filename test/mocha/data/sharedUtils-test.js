913f0574e77dc76fb8d3da5bc4adf76fcf519115
Benedikt Gross <bgfodw@gmx.de>
2017-02-27 20:36:12 +0100

    added more test for formatString

--- test/mocha/data/sharedUtils-test.js
+++ test/mocha/data/sharedUtils-test.js
@@ -6,6 +6,14 @@ describe('sharedUtils',function () {
        it('should replace placeholders with correct objects',function () {
            sharedUtils.formatString("XXX{0}XXX{1}XXX","0","1").should.equal('XXX0XXX1XXX');
            sharedUtils.formatString("XXX{1}XXX{0}XXX","0","1").should.equal('XXX1XXX0XXX');
-       })
+       });
+       
+       it('should accept empty parameters',function () {
+           sharedUtils.formatString('XYZ').should.equal('XYZ');
+       });
+
+       it('should accept empty string',function () {
+           sharedUtils.formatString('').should.equal('');
+       });
    }) 
 });
