185e0238db27ca0150042d32e4e5481945f6c9b5
Benedikt Gross <bgfodw@gmx.de>
2017-03-21 18:51:22 +0100

    removed outdated unit tests and renamed the directories to be more expressive

---
+++ test/unit-tests/lib/Badge-test.js
@@ -0,0 +1,95 @@
+const sourcepath = "../../../";
+
+const should = require('should');
+const Badge = require(sourcepath + 'lib/Badge');
+
+function createButton() {
+    return {
+        badge: '',
+        badgeColor: ''
+    };
+}
+
+describe('Badge', function () {
+    describe('init', function () {
+        it('should initialize the badge', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            button.badge.should.not.be.equal('');
+            button.badgeColor.should.not.be.equal('');
+        });
+    });
+
+    describe('set', function () {
+        it('should reset the badge if value is zero', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.set(0);
+            button.badge.should.be.equal('ok');
+        });
+
+        it('should set the badge to the provided value', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.set(5);
+            button.badge.should.be.equal(5);
+        });
+    });
+
+    describe('increment', function () {
+        it('should increment badge', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.increment();
+            button.badge.should.be.equal(1);
+        });
+    });
+
+    describe('decrement', function () {
+        it('should not decrement below zero', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.decrement();
+            button.badge.should.be.equal('ok');
+
+            badge.increment();
+            button.badge.should.be.equal(1);
+        });
+
+        it('should reset if resulting value is zero', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.set(1);
+            badge.decrement();
+            button.badge.should.be.equal('ok');
+        });
+
+        it('should decrement the value', function () {
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.set(5);
+            badge.decrement();
+            button.badge.should.be.equal(4);
+        })
+    });
+
+    describe('activateRecording',function () {
+        it('should activate recording icon',function(){
+        let button = createButton();
+        let badge = new Badge(button);
+        badge.activateRecording();
+        button.badge.should.be.equal('rec');
+        });
+    });
+
+    describe('deactivateRecording',function () {
+        it('should deactivate recording icon and restore previous',function(){
+            let button = createButton();
+            let badge = new Badge(button);
+            badge.set(11);
+            badge.activateRecording();
+            badge.deactivateRecording();
+            button.badge.should.be.equal(11);
+        });
+    });
+});
---
+++ test/unit-tests/lib/Utils-test.js
@@ -0,0 +1,54 @@
+const sourcepath = '../../../';
+
+const proxyquire = require('proxyquire').noCallThru();
+const HashTable = require(sourcepath + 'lib/Hashtable');
+const should = require('should');
+
+const utils = proxyquire(sourcepath + 'lib/Utils', {
+    'lib/Hashtable': HashTable
+});
+
+describe('Utils', function () {
+    describe('getMainpageFromLink', function () {
+        it('should extract host correctly', function () {
+            utils.getMainPageFromLink("https://www.facebook.com/settings/password").should.be.equal("https://www.facebook.com")
+        });
+
+        it('should be empty for empty input', function () {
+            utils.getMainPageFromLink("").should.be.equal("");
+        });
+    });
+
+    describe('removeTrailingSlash', function () {
+        it('should remove trailing slash', function () {
+            utils.removeTrailingSlash('https://www.url.com/').should.be.equal('https://www.url.com');
+        });
+
+        it('should leave url as is if no trailing slash is present', function () {
+            let url = 'https://www.url.com';
+            utils.removeTrailingSlash(url).should.be.equal(url);
+        });
+    });
+
+    describe('arraysEqual', function () {
+        it('should not throw error on null', function () {
+            utils.arraysEqual(null,[]).should.be.false();
+            utils.arraysEqual([],null).should.be.false();
+            utils.arraysEqual(null,[1]).should.be.false();
+            utils.arraysEqual([1],null).should.be.false();
+        });
+        it('should return true if both are null', function () {
+            utils.arraysEqual(null,null).should.be.true();
+        });
+        it('should return false if arrays have different lengths', function () {
+            utils.arraysEqual([1],[1,2]).should.be.false();
+        });
+        it('should return true if arrays are equal', function () {
+            utils.arraysEqual([1,4,3], [1,4,3]).should.be.true();
+        });
+        it('should return false if arrays are not equal', function () {
+            utils.arraysEqual([1,4,3], [1,1,2]).should.be.false();
+            utils.arraysEqual([1,4,3], [1,1,1]).should.be.false();
+        });
+    });
+});
