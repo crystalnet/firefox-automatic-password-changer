c1ee198cbdb9c22fcf5b82bf10759c1a8ffd4420
crystalneth <konsti7@gmx.net>
2017-06-11 00:56:32 +0200

    added tests; added wireframe of player

--- test/test.js
+++ test/test.js
@@ -2,13 +2,40 @@
  * Created by crystalneth on 10-Jun-17.
  */
 
-var should = require('should');
+const should = require('should');
 describe('Player', function () {
-    describe('#test()', function () {
-        it('should test test test', function () {
-            var Player = require('../webextension/background-scripts/player.js');
-            var player = new Player('');
-            player.test(true).should.be.ok;
+
+    const Player = require('../webextension/background-scripts/player.js');
+    const data = '[{"allowedCharacterSets":{"az":"abcdefghijklmnopqrstuvwxyz","AZ":"ABCDEFGHIJKLMNOPQRSTUVWXYZ","num":"0123456789","special":"!@#$%^*._"},"minLength":8,"compositionRequirements":[{"kind":"mustNot","num":1,"rule":{"description":"May not be the same as your username or contain your username.","regexp":".*[username].*"}},{"kind":"must","num":1,"rule":{"description":"Must contain at least one number.","regexp":".*[num].*"}},{"kind":"must","num":1,"rule":{"description":"Must contain at least one lower case letter.","regexp":".*[az].*"}},{"kind":"must","num":1,"rule":{"description":"Must contain at least one upper case letter.","regexp":".*[AZ].*"}},{"kind":"must","num":1,"rule":{"description":"Must contain at least one special character.","regexp":".*[special].*"}},{"kind":"mustNot","num":1,"rule":{"description":"The special character cannot be the first character in the password.","regexp":"^[special].*"}},{"kind":"mustNot","num":5,"rule":{"description":"May not be the same as any of the 5 previous passwords used.","regexp":"^[password]"}}]}]';
+    const player = new Player(data);
+
+    describe('#_parseBlueprint()', function () {
+        it('should have a to z property', function () {
+            player._parseBlueprint(data)[0].allowedCharacterSets.az.should.equal('abcdefghijklmnopqrstuvwxyz');
+        });
+
+        it('should composition requirements as array', function () {
+            player._parseBlueprint(data)[0].compositionRequirements[0].rule.description.should.equal('May not be the same as your username or contain your username.');
+        });
+    });
+
+    describe('#_invokePasswordGenerator()', function () {
+        it('should return a password', function () {
+            player._invokePasswordGenerator().should.be.a.String();
+        });
+
+        it('should return a password of at least 8 symbols', function () {
+            player._invokePasswordGenerator().length.should.be.aboveOrEqual(8);
+        });
+    });
+
+    describe('#_validateGeneratedPassword()', function () {
+        it('should reject a empty password', function () {
+            player._validatePassword('').should.not.be.ok;
+        });
+
+        it('should accept a valid password', function () {
+            player._validatePassword('12345678Ab$').should.be.ok;
         });
     });
 });
--- webextension/background-scripts/player.js
+++ webextension/background-scripts/player.js
@@ -4,12 +4,43 @@
 
 class Player {
     // TODO documentation
-    constructor(blueprint) {
-        this.blueprint = blueprint;
+    constructor(blueprintJson) {
+        this.schema = JSON.parse('{"$schema":"http://json-schema.org/schema#","title":"Password Composition Policy","description":"Array of password policy descriptions for the automatic creation of new passwords, DRAFT 2017-02-10","id":"URI TBD","type":"array","items":{"type":"object","properties":{"allowedCharacterSets":{"type":"object","description":"The different sets of allowed characters. Threre are special charsets available to all policies: username (is filled with the username if available), emanresu (is filled with the reverse username if available), allASCII (represents all ASCII characters), allUnicode (represents all Unicode characters). The names of these special character sets must not be used by other charset definitions.","minProperties":1},"minLength":{"type":"number","description":"The minimum length of the password, if left out: assumed to be 1","minimum":1},"maxLength":{"type":"number","description":"The maximum length of the password, if left out: assumed to be infinite","minimum":1},"compositionRequirements":{"type":"array","description":"The list of composition requirements in this password policy. If left out: assumed that all character sets can be used in any combination.","items":{"type":"object","description":"Representations of composition requirements using rules (regexps) on the allowed character sets, which either must or must not be fulfilled by valid passwords.","required":["kind","num","rule"],"properties":{"kind":{"type":"string","enum":["must","mustNot"]},"num":{"type":"number"},"rule":{"type":"object","description":"The rule of this composition requirement as regexp.","properties":{"description":{"type":"string","description":"A textual description of the rule to display to the user in the UI."},"regexp":{"type":"string","description":"The actual regexp of the rule."}}}},"minItems":1,"uniqueItems":true}}}}}');
+        this.blueprint = this._parseBlueprint(blueprintJson, this.schema);
     }
 
-    test(a) {
-        return Boolean(a);
+    // Bekommen wir einen JSON-String oder ein JSON-Objekt?
+    _parseBlueprint(blueprintJson) {
+        const Ajv = require('ajv');
+        const ajv = new Ajv();
+        const validate = ajv.compile(this.schema);
+        const blueprint = JSON.parse(blueprintJson);
+
+        if (validate(blueprint)) {
+            return blueprint;
+        } else {
+            console.log(validate.errors);
+            throw 'Invalid blueprint';
+        }
+    }
+
+    _invokePasswordGenerator() {
+        const passwordGenerator = new PasswordGen();
+        // TODO validate and regenerate password
+        let length = this.blueprint[0].minLength;
+        const allowsCharacterSets = this.blueprint[0].allowedCharacterSets;
+        let alphabet = allowsCharacterSets.az + allowsCharacterSets.AZ + allowsCharacterSets.num +  allowsCharacterSets.special;
+        this.password = passwordGenerator.generatePassword(length, alphabet);
+    }
+
+    _validatePassword(password) {
+        for (let requirement in this.blueprint[0].compositionRequirements) {
+            let rule = requirement.rule.regexp;
+            if (!rule.test(password)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
