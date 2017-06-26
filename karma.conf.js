3347931cd1107abbc888a490d61d4b67618cdcd5
Konstantin Schmidt <ko.schmidt@posteo.de>
2017-06-26 12:37:00 +0200

    added karma support with mocha and shouldjs

---
+++ karma.conf.js
@@ -0,0 +1,78 @@
+// Karma configuration
+// Generated on Sun Jun 25 2017 10:10:01 GMT+0200 (Mitteleuropäische Sommerzeit)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'should'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        // Test dependencies
+        'node_modules/expect.js/index.js',
+        'node_modules/sinon-chrome/bundle/sinon-chrome-webextensions.min.js',
+
+        // Source
+        'webextension/background-scripts/passwordGenerator.js',
+        'webextension/background-scripts/player.js',
+        'webextension/external-scripts/argon2.js',
+        'webextension/external-scripts/salsa20.js',
+        'webextension/external-scripts/argon2-asm.min.js',
+        'webextension/external-scripts/lodash_throttle.min.js',
+
+        // Tests
+        'test/*.js'
+    ],
+
+      client: {
+          mocha: {
+              // change Karma's debug.html to the mocha web reporter
+              reporter: 'html',
+          },
+      },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
