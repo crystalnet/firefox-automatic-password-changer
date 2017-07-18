d1b588714c94fdf686ea8d5c558c7c60c7512681
crystalneth <konsti7@gmx.net>
2017-07-18 21:03:48 +0200

    better reporting of coverage

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'webextension/**/*.js': ['coverage']
+            'webextension/background-scripts/player.js': ['coverage']
         },
 
         junitReporter: {
